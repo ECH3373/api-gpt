0c442339213c91e5efa65199f0ca9d64975576b6
Eduardo Cortés Herrera <eduardocortes3373@gmail.com>
2025-04-14 08:34:42 -0600

    Fix function and function_call

--- src/chat/controller.js
+++ src/chat/controller.js
@@ -3,11 +3,11 @@ import { config } from '../../config/index.js';
 import { services } from '../shared/services/index.js';
 
 const message = async (req, res) => {
-  const { model = 'gpt-3.5-turbo', prompt = '', role = 'user', message = '', history = [] } = req.body;
+  const { model = 'gpt-3.5-turbo', prompt = '', role = 'user', message = '', history = [], tools = [] } = req.body;
   const gpt = new OpenAI({ apiKey: config.gpt.key });
   const messages = [{ role: 'system', content: prompt }, ...history, { role, content: message }];
   history.push({ role, content: message });
-  const completion = await gpt.chat.completions.create({ messages, model });
+  const completion = await gpt.chat.completions.create({ messages, model, functions: tools });
   const response = completion.choices[0].message;
   history.push(response);
   const data = { response, history };
